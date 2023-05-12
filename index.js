320476af23ac37d33e5b8faa39ca29085de6a4d6
Tathagata Chakraborty <tathagata1805@gmail.com>
2023-05-12 20:10:48 +0530

    adding comments

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ import readline from "readline";
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+// CREATING OPENAI CONFIGURATION USING THE API KEY
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API,
 });
+
+// CREATE AN OPENAI INSTANCE USING THE CONFIGURATION ABOVE 
 const openai = new OpenAIApi(configuration);
 
+// CONFIGURATION FOR I/O SETUP IN THE TERMINAL
 const userInterface = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+// GETTING THE USER PROMPT
 userInterface.prompt();
 
+// I/O SETUP BETWEEN USER AND CHATBOT
 userInterface.on("line", async (input) => {
   await openai
     .createChatCompletion({
